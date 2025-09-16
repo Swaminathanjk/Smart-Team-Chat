57449a3639c957b9a7143b28248e79d7246085af
Swaminathanjk <swaminathanjk@gmail.com>
2025-09-16 10:31:27 +0530

    rem additional header

--- debug/test.js
+++ debug/test.js
@@ -1,1 +1,8 @@
-console.log(new Date().toString());
+console.log(new Date().toLocaleString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            month: "2-digit",
+            day: "2-digit",
+            year: "2-digit",
+          }));
